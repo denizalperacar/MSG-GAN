ad4d8ce60f378bd3894dcc5bdcf9751bd56fa013
Deniz A. Acar <59959106+denizalperacar@users.noreply.github.com>
2021-05-30 01:06:57 +0300

    train final version

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from torchvision.transforms.transforms import Normalize
 from torchvision.utils import save_image
 from os import getcwd
 from time import time, sleep
-
+import numpy as np
 from src.loss import WGANGP_loss
 from src.model import Generator, Discriminator
 from src.custom import LossTracker, get_latent_variable
@@ -21,15 +21,12 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("--num_epochs", type=int, default=100, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
-parser.add_argument("--lr", type=float, default=0.00001, help="RMSPROP: learning rate")
-parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--latent_dim", type=int, default=512, help="dimensionality of the latent space")
-parser.add_argument("--num_blocks_gen", type=int, default=4, help="number of generator blocks")
-parser.add_argument("--num_blocks_dis", type=int, default=4, help="number of discriminator blocks")
+parser.add_argument("--lr", type=float, default=0.003, help="RMSPROP: learning rate")
+parser.add_argument("--latent_dim", type=int, default=128, help="dimensionality of the latent space")
+parser.add_argument("--num_blocks", type=int, default=4, help="number of generator blocks")
 parser.add_argument("--use_gpu", type=bool, default=True, help="Use GPU for training")
 parser.add_argument("--save_skips", type=int, default=200, help="number of epochs to skip saving the model")
-parser.add_argument("--continue_checkpoint", type=bool, default=True, help="Continue from the last checkpoint")
+parser.add_argument("--continue_checkpoint", type=bool, default=False, help="Continue from the last checkpoint")
 parser.add_argument("--save_dir", type=str, default=f"{getcwd()}/weights/", help="Continue from the last checkpoint")
 parser.add_argument("--images", type=str, default=f"{getcwd()}/images/", help="Continue from the last checkpoint")
 parser.add_argument("--n_disc", type=int, default=1, help="the number of discriminator iterations per generator iteration")
@@ -51,15 +48,18 @@ if not os.path.exists(opt.save_dir):
     os.mkdir(opt.save_dir)
 if not os.path.exists(opt.images):
     os.mkdir(opt.images)
-if not os.path.exists(opt.images):
-    os.mkdir(opt.images)
-    
 
 device = torch.device("cuda:0") if opt.use_gpu else torch.device("cpu") 
 
 # create the models
-generator = Generator(opt.num_blocks_gen).to(device)
-discriminator = Discriminator(opt.num_blocks_dis).to(device)
+generator = Generator(
+    opt.num_blocks,
+    channels=np.array([512,512,512,512,512,256,128,64,32,16]).astype(int)//4
+    ).to(device)
+discriminator = Discriminator(
+    opt.num_blocks,
+    channels=np.array([16, 32, 64, 128, 256, 512, 512, 512, 512, 512]).astype(int)//4
+    ).to(device)
 
 # Load the models if necessary
 if opt.continue_checkpoint:
@@ -78,7 +78,7 @@ discriminator_optimizer = RMSprop(
 
 # loading the data
 data_loader = DataLoader(
-    opt.dataset, opt.batch_size, opt.num_blocks_dis, device
+    opt.dataset, opt.batch_size, opt.num_blocks, device
     )
 
 # track loss stats of the model
@@ -106,10 +106,6 @@ while not converged:
             )
 
         fake_images = generator(latent_variable)
-
-        #for i in fake_images.keys():
-        #    print(fake_images[i].mean())
-
         real_images = data_loader.load_images()
 
         discriminator_optimizer.zero_grad()
@@ -146,14 +142,12 @@ while not converged:
     
     if counter % opt.gen_steps == 0:
         
-        if counter % (opt.gen_steps) == 0:
-            # TODO implement this
-            
-            latent_variable = get_latent_variable(
-                20, opt.latent_dim, device
-                )
-            fake_images = generator(latent_variable)[opt.num_blocks_gen-1].to("cpu")
-            save_image(fake_images.data[:20], f"{opt.images}/%d.png" % counter, nrow=5, value_range=(-1,1), normalize=True)
+
+        latent_variable = get_latent_variable(
+            20, opt.latent_dim, device
+            )
+        fake_images = generator(latent_variable)[opt.num_blocks-1].to("cpu")
+        save_image(fake_images.data[:20], f"{opt.images}/%d.png" % counter, nrow=5, value_range=(-1,1), normalize=True)
         
         # statistics of discriminator
         print(print_string.format(
